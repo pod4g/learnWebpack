929196197b3ecb79acf10676326d1dc6ffe6ba87
lyf <1601942611@qq.com>
2016-11-07 10:47:19 +0800

    update

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,2 +1,4 @@
-alert('home.js')
 console.log('home.js');
+require('../styles/home.scss')
+
+export default 1;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
-const src = path.resolve(__dirname, 'src')
+const ROOT_PATH = path.resolve(__dirname);
+const APP_PATH = path.resolve(ROOT_PATH, 'src')
+const BUILD_PATH = path.resolve(ROOT_PATH, 'dist')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 module.exports = {
   // 必须得定义context，否则的话，会在__dirname下
   // 生成assets
-  context: src,
+  context: APP_PATH,
   entry: {
     // home: path.join(src,'scripts/home'),
     // detail: path.join(src,'scripts/detail')
@@ -12,10 +15,26 @@ module.exports = {
   },
   output: {
     // path: 'assets', // 这个不行
-    path: path.join(src,'assets'),
+    path: path.join(APP_PATH,'assets'),
     publicPath: '/static/build/',
     filename: '[name].js',
     chunkFilename: 'bundle-[id].js'
+  },
+  module: {
+    loaders: [{
+        test: /\.scss$/,
+        loaders: ['style','css','sass']
+      }, {
+          test: /\.jsx?$/,
+          loader: 'babel',
+          include: APP_PATH,
+          query: {
+            presets: ['es2015']
+          }
+      }],
+    plugins: [
+      new ExtractTextPlugin("[name].css")
+    ]
   },
   resolve: {
     // 告诉webpack所有模块的启始目录由当前目录开始，再执行下webpack就可以正常输出了：
